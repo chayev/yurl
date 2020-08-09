5b1fab53e43cd7d0f25e16bdffb390087e09bab7
Michael Ibragimchayev <michael.ibragimchayev@gmail.com>
2020-08-08 20:07:20 -0400

    Connect webapp to validation logic

--- webapp/main.go
+++ webapp/main.go
@@ -14,42 +14,56 @@ func main() {
 
 	log.Println("Listening on :8080...")
 	http.HandleFunc("/", handler)
-	http.HandleFunc("/main", viewMain)
+	http.HandleFunc("/results", viewResults)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
+// PageOutput : The contents and URL parameters that are exported
 type PageOutput struct {
 	Content string
+	URL     string
+	Prefix  string
+	Bundle  string
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
 
-	input := "https://kohls.onelink.me/asdas"
-
-	output := yurllib.CheckDomain(input, "", "", true)
-
-	var content PageOutput
-
-	for _, item := range output {
-		content.Content += item
-	}
-
 	t, _ := template.ParseFiles("tpl/base.html")
-	t.Execute(w, &content)
+	t.Execute(w, nil)
 }
 
-func viewMain(w http.ResponseWriter, r *http.Request) {
+func viewResults(w http.ResponseWriter, r *http.Request) {
 
-	input := "https://kohls.onelink.me/asdas"
+	var url string
+	var prefix string
+	var bundle string
 
-	output := yurllib.CheckDomain(input, "", "", true)
+	for _, n := range r.URL.Query()["url"] {
+		url = n
+	}
 
-	var content PageOutput
+	for _, n := range r.URL.Query()["prefix"] {
+		prefix = n
+	}
+
+	for _, n := range r.URL.Query()["bundle"] {
+		bundle = n
+	}
+
+	var output []string
+
+	if url == "" {
+		output = append(output, "Enter URL to validate.")
+	} else {
+		output = yurllib.CheckDomain(url, prefix, bundle, true)
+	}
+
+	content := &PageOutput{URL: url, Prefix: prefix, Bundle: bundle}
 
 	for _, item := range output {
 		content.Content += item
 	}
 
-	t, _ := template.ParseFiles("tpl/main.html")
+	t, _ := template.ParseFiles("tpl/results.html")
 	t.Execute(w, &content)
 }
