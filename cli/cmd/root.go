3e038a0f1312a66743ba3fad9f6d4ffcce000636
Michael Ibragimchayev <michael.ibragimchayev@gmail.com>
2020-08-28 20:28:23 -0400

    Add version command.

--- cli/cmd/root.go
+++ cli/cmd/root.go
@@ -9,15 +9,11 @@ import (
 
 var cfgFile string
 
-// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:     "yurl",
-	Short:   "yURL: Validate whether your URL is enabled properly",
-	Version: "v0.2.0",
+	Use:   "yurl",
+	Short: "yURL: Validate whether your URL is enabled properly",
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
@@ -26,10 +22,5 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
+	cobra.OnInitialize()
 }
